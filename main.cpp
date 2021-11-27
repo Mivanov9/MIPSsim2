e1af3fd7e750e04dd0ed9c486607ba379a467b33
Michael Ivanov <ivanovmichael@ufl.edu>
2021-11-26 19:13:14 -0500

    added issue, ALU2 and WB

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <iomanip>
 #include <deque>
+#include <algorithm>
 
 enum OpCodes {
     J, BEQ, BNE, BGTZ, SW, LW, BREAK, ADD, SUB, AND, OR, SRL, SRA, MUL, ADDI, ANDI, ORI
@@ -17,9 +18,11 @@ struct Instruction {
     OpCodes opCode;
     int category;
     std::vector<int> args;
+    int address;
 
-    Instruction(std::string name, OpCodes opCode, int category, std::vector<int> args);
+    Instruction(std::string name, int address, OpCodes opCode, int category, std::vector<int> args);
 };
+
 class State {
     bool foundBreak = false;
     Instruction *currentInstruction = nullptr;
@@ -46,15 +49,19 @@ public:
     int address = 260;
     int firstDataAddress = 0;
     void fetch(std::unordered_map<int, Instruction> &instructions);
-    void issue();
+    void issue(State &prevStat, std::array<bool, 32> &registersWritten);
     void loadAndStore();
-    void arithmetic();
+    void arithmetic(State &prevStat);
     void multiply();
     void writeBack();
     void writeRegisters(std::ofstream &simFile);
     void writeData(std::ofstream &simFile);
     void writeState(std::ofstream &simFile);
+
+    void cleanUp();
 };
+
+void removeInstruction(std::deque<Instruction*> &buf, int address);
 
 void State::calculate() {
     switch (currentInstruction->category) {
@@ -71,7 +78,7 @@ void State::calculate() {
 }
 bool State::isBranch(Instruction &instruction) {
     OpCodes op = instruction.opCode;
-    return op == BEQ || op == BNE || op == BGTZ;
+    return op == J || op == BEQ || op == BNE || op == BGTZ;
 }
 
 void State::fetch(std::unordered_map<int, Instruction> &instructions) {
@@ -89,16 +96,65 @@ void State::fetch(std::unordered_map<int, Instruction> &instructions) {
     }
 }
 
-void State::issue() {
-
+void State::issue(State &prevStat, std::array<bool, 32> &registersWritten) {
+    auto &pBuf1 = prevStat.buf1;
+    auto &prevBuf2 = prevStat.buf2;
+    auto &prevBuf3 = prevStat.buf3;
+    auto &prevBuf4 = prevStat.buf4;
+    auto pBuf1Iter = pBuf1.begin();
+    for (int i = 0; i < 6; ++i, ++pBuf1Iter) {
+        if (pBuf1Iter == pBuf1.end())
+            return;
+        auto op = (*pBuf1Iter)->opCode;
+        int dest = (*pBuf1Iter)->args[0];
+        int src1 = (*pBuf1Iter)->args[1];
+        int src2 = (*pBuf1Iter)->args[2];
+        if (op != LW && op != SW) {
+            // RAW
+            if (registersWritten[src1] || registersWritten[src2])
+                continue;
+            if (op == MUL) {
+                if (buf4.size() >= 2) {
+                    continue;
+                }
+                buf4.push_back(*pBuf1Iter);
+            } else {
+                if (buf3.size() >= 2) {
+                    continue;
+                }
+                buf3.push_back(*pBuf1Iter);
+            }
+            removeInstruction(buf1, (*pBuf1Iter)->address);
+//            auto findAddress = [pBuf1Iter](Instruction *instruction) {return instruction->address == (*pBuf1Iter)->address;};
+//            auto removeIter = std::find_if(buf1.begin(), buf1.end(), findAddress);
+//            buf1.erase(removeIter);
+            registersWritten[dest] = true;
+            continue;
+        }
+        if (op == LW) {
+            auto SWLambda = [](Instruction* instruction) {return instruction->opCode == SW;};
+            if (std::any_of(pBuf1.begin(), pBuf1Iter, SWLambda))
+                continue;
+        }
+    }
 }
 
 void State::loadAndStore() {
 
 }
 
-void State::arithmetic() {
+void removeInstruction(std::deque<Instruction*> &buf, int address) {
+    auto findAddress = [address](Instruction *instruction) {return instruction->address == address;};
+    auto removeIter = std::find_if(buf.begin(), buf.end(), findAddress);
+    buf.erase(removeIter);
+}
 
+void State::arithmetic(State &prevStat) {
+    auto &pBuf3 = prevStat.buf3;
+    if (pBuf3.empty())
+        return;
+    buf6 = pBuf3.front();
+    removeInstruction(buf3, buf6->address);
 }
 
 void State::multiply() {
@@ -106,7 +162,24 @@ void State::multiply() {
 }
 
 void State::writeBack() {
+    if (buf8) {
+        currentInstruction = buf8;
+        calculate();
+    }
+    if (buf6) {
+        currentInstruction = buf6;
+        calculate();
+    }
+    if (buf10) {
+        currentInstruction = buf10;
+        calculate();
+    }
+}
 
+void State::cleanUp() {
+    buf8 = nullptr;
+    buf6 = nullptr;
+    buf10 = nullptr;
 }
 
 void State::calculate1() {
@@ -195,8 +268,8 @@ void State::calculate3() {
     }
 }
 
-Instruction::Instruction(std::string name, OpCodes opCode, int category, std::vector<int> args) :
-        name(std::move(name)), opCode(opCode), category(category), args(std::move(args)) {}
+Instruction::Instruction(std::string name, int address, OpCodes opCode, int category, std::vector<int> args) :
+        name(std::move(name)), address(address), opCode(opCode), category(category), args(std::move(args)) {}
 
 struct OpCodeMap {
     const std::unordered_map<std::string, std::pair<std::string, OpCodes>> cat1 = {
@@ -268,7 +341,7 @@ void category1(OpCodeMap &opCodeMap, const std::string &line,
         default:
             break;
     }
-    Instruction instruction(instructionString.str(), opCodePair.second, 1, {src1, src2, src3});
+    Instruction instruction(instructionString.str(), address, opCodePair.second, 1, {src1, src2, src3});
     instructions.emplace(address, instruction);
 
 }
@@ -290,7 +363,7 @@ void category2(OpCodeMap &opCodeMap, const std::string &line,
         default:
             instructionString << "R" << dest << ", R" << src1 << ", R" << src2;
     }
-    Instruction instruction(instructionString.str(), opCodePair.second, 2, {dest, src1, src2});
+    Instruction instruction(instructionString.str(), address, opCodePair.second, 2, {dest, src1, src2});
     instructions.emplace(address, instruction);
 }
 
@@ -307,10 +380,9 @@ void category3(OpCodeMap &opCodeMap, const std::string &line,
         immediate = (int16_t) immediate;
     }
     instructionString << "R" << dest << ", R" << src1 << ", #" << immediate;
-    Instruction instruction(instructionString.str(), opCodePair.second, 3, {dest, src1, immediate});
+    Instruction instruction(instructionString.str(), address, opCodePair.second, 3, {dest, src1, immediate});
     instructions.emplace(address, instruction);
 }
-
 void State::writeRegisters(std::ofstream &simFile) {
     simFile << "Registers\n";
     for (int i = 0; i < 4; ++i) {
@@ -321,31 +393,49 @@ void State::writeRegisters(std::ofstream &simFile) {
         simFile << '\n';
     }
 }
+
 void State::writeData(std::ofstream &simFile) {
+    int currAddress = firstDataAddress;
     simFile << "\nData";
     for (size_t i = 0; i < data.size(); ++i) {
         if (i % 8 == 0)
-            simFile << "\n" << firstDataAddress << ":";
-        simFile << "\t" << data[firstDataAddress];
-        firstDataAddress += 4;
+            simFile << "\n" << currAddress << ":";
+        simFile << "\t" << data[currAddress];
+        currAddress += 4;
     }
     simFile << "\n\n";
 }
 
 void State::writeState(std::ofstream &simFile) {
     simFile << "IF:\n";
-    simFile << "\tWaiting: [" << (waitInstruction == nullptr ? "" : waitInstruction->name) << "]\n";
-    simFile << "\tExecuted: [" << (exeInstruction == nullptr ? "" : exeInstruction->name) << "]\n";
-    simFile << "Buf1:\n";
-    auto iter = buf1.begin();
-    for (int i = 0; i < 8; ++i) {
-        simFile << "\tEntry " << i << ':';
-        if (iter != buf1.end()) {
-            simFile << " [" << (*iter)->name << ']';
-            iter++;
+    simFile << "\tWaiting:" << (waitInstruction == nullptr ? "" : " [" + waitInstruction->name + ']') << '\n';
+    simFile << "\tExecuted:" << (exeInstruction == nullptr ? "" : " [" + exeInstruction->name + ']') << '\n';
+    auto writeBufLambda = [&simFile](int size, std::deque<Instruction*> &buf){
+        auto iter = buf.begin();
+        for (int i = 0; i < size; ++i) {
+            simFile << "\tEntry " << i << ':';
+            if (iter != buf.end()) {
+                simFile << " [" << (*iter)->name << ']';
+                iter++;
+            }
+            simFile << '\n';
         }
-        simFile << '\n';
-    }
+    };
+    simFile << "Buf1:\n";
+    writeBufLambda(8, buf1);
+    simFile << "Buf2:\n";
+    writeBufLambda(2, buf2);
+    simFile << "Buf3:\n";
+    writeBufLambda(2, buf3);
+    simFile << "Buf4:\n";
+    writeBufLambda(2, buf4);
+    simFile << "Buf5:" << (buf5 ? " [" + buf5->name + ']' : "") << '\n';
+    simFile << "Buf6:" << (buf6 ? " [" + buf6->name + ']' : "") << '\n';
+    simFile << "Buf7:" << (buf7 ? " [" + buf7->name + ']' : "") << '\n';
+    simFile << "Buf8:" << (buf8 ? " [" + buf8->name + ']' : "") << '\n';
+    simFile << "Buf9:" << (buf9 ? " [" + buf9->name + ']' : "") << '\n';
+    simFile << "Buf10:" << (buf10 ? " [" + buf10->name + ']' : "") << '\n';
+    simFile << '\n';
 }
 
 void decodeData(State &state, std::vector<std::string> &binaryInstructions, OpCodeMap &opCodeMap, std::unordered_map<int, Instruction> &instructions) {
@@ -397,21 +487,24 @@ int main(int argc, char** argv) {
 
     int cycle = 1;
 
-    while (true) {
+    State prevState = state;
+    std::array<bool, 32> registersWritten = {};
+    for (int i = 0; i < 20; ++i) {
         Instruction instruction = instructions.at(address);
         simFile << std::string(20, '-') << '\n';
         simFile << "Cycle " << cycle << ':' << "\n\n";
         state.fetch(instructions);
-        state.issue();
+        state.issue(prevState, registersWritten);
         state.loadAndStore();
-        state.arithmetic();
+        state.arithmetic(prevState);
         state.multiply();
         state.writeBack();
         state.writeState(simFile);
         state.writeRegisters(simFile);
         state.writeData(simFile);
+        state.cleanUp();
+        prevState = state;
         cycle++;
-        break;
     }
     simFile.close();
     return 0;
